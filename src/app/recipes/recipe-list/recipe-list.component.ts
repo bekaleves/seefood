55f34fac57f2fdb8fc9dee6ba6babc67ddecc91e
Németh Dániel <daniel.nemeth@frontendart.com>
2020-07-22 19:15:56 +0200

    0722 home

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.thespruceeats.com/thmb/tXKqu9A5bWpYiMzl2k2ZvEhsS9Q=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/cook-and-keep-octopus-tender-2018813-14_preview-HORZ-5b23ef75a9d4f900374697a3-9e7b5b30b36c494ea7c6987809bf03c1.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.thespruceeats.com/thmb/tXKqu9A5bWpYiMzl2k2ZvEhsS9Q=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/cook-and-keep-octopus-tender-2018813-14_preview-HORZ-5b23ef75a9d4f900374697a3-9e7b5b30b36c494ea7c6987809bf03c1.jpg')
+  ];
 
   constructor() { }
 
